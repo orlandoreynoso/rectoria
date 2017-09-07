a1694844ab7338e2585bd1ea365cf6721ea6ab93
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-09-07 05:53:45 -0600

    start template

---
+++ libs/infoparroquial.php
@@ -0,0 +1,29 @@
+<div class="titulo-iglesia">
+	<h3>Anuncios parroquiales</h3>
+</div>
+<div class="c-info" id="iglesia">
+	<div id="div1">
+		<a href="<?php bloginfo('url'); ?>/horarios-de-misa">
+			<img src="<?php echo IMAGES.'/jesus.jpg'; ?>" alt="">
+			<span>Horario de misa</span>					
+		</a>
+	</div>
+	<div id="div1">
+		<a href="<?php bloginfo('url'); ?>/boletin-parroquial/">
+			<img src="<?php echo IMAGES.'/boletin-parroquial.jpg'; ?>" alt="">
+			<span>Boletines parroquiales</span>
+		</a>
+	</div>
+	<div id="div1">
+		<a href="<?php bloginfo('url'); ?>/editoriales/">
+			<img src="<?php echo IMAGES.'/pluma.jpg'; ?>" alt="">
+			<span>Editoriales</span>
+		</a>
+	</div>
+	<!-- div id="div1">
+		<a href="<?php // bloginfo('url'); ?>/rectoria-de-santa-rita/">
+			<img src="<?php // echo IMAGES.'/santarita.png'; ?>" alt="">
+			<span>Rectoria de Santa Rita</span>
+		</a>
+	</div -->
+</div>
---
+++ libs/recomendaciones.php
@@ -0,0 +1,19 @@
+<div class="titulo-recomendaciones">
+	<h3>Recomendaciones</h3>
+</div>
+<div class="c-recomendaciones">
+<?php /*
+	<?php get_laterales(1385,1,'Año de la misericordia','reflexiones'); ?>
+	<?php get_laterales(1294,1,'Reflexiones Sacerdotales','reflexiones'); ?>			
+	<?php get_laterales(1254,1,'Conozca Guatemala','reflexiones'); ?>
+	<?php get_laterales(692,1,'Presentaciones','reflexiones'); ?>
+	<?php get_laterales(737,1,'El atrio','reflexiones'); ?>
+*/ 
+//get_recomendaciones_name('atrio');
+get_recomendaciones_ihome(1385,1,'Año de la misericordia');
+get_recomendaciones_ihome(1294,1,'Reflexiones Sacerdotales');
+get_recomendaciones_iname('conozca-guatemala'); 
+get_recomendaciones_ihome(692,1,'Presentaciones');
+get_recomendaciones_iname('atrio'); 
+?>	
+</div>
